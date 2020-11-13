853fa3b53dfa0329f2610bf261b39e7ad7358599
Phillip Hoff <phillip@orst.edu>
2020-11-13 15:51:35 -0800

    Add service filtering when debugging.

--- src/debug/tyeDebugConfigurationProvider.ts
+++ src/debug/tyeDebugConfigurationProvider.ts
@@ -1,20 +1,29 @@
-import { TyeClient } from 'src/services/tyeClient';
 import * as vscode from 'vscode';
+import { TyeClient } from 'src/services/tyeClient';
+
+export interface TyeDebugConfiguration extends vscode.DebugConfiguration {
+    services?: string[];
+}
 
 export class TyeDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
     constructor(private readonly tyeClient: TyeClient) {
     }
 
     async resolveDebugConfigurationWithSubstitutedVariables(folder: vscode.WorkspaceFolder | undefined, debugConfiguration: vscode.DebugConfiguration, token?: vscode.CancellationToken): Promise<vscode.DebugConfiguration | null | undefined> {
+        const tyeDebugConfiguration = <TyeDebugConfiguration>debugConfiguration;
+
         const services = await this.tyeClient.getServices();
-        if(services) {
-            for(const service of services) {
-                if(service.serviceType === 'project') {
-                    for(const replicaName of Object.keys(service.replicas)) {
-                        const config = {type:'coreclr', name:`Attach to Tye PID: ${service.replicas[replicaName].pid}`,request:'attach', processId:`${service.replicas[replicaName].pid}`};
-                        await vscode.debug.startDebugging(undefined, config);
-                    }
-                }
+
+        for (const debuggedService of (services ?? []).filter(service => service.serviceType === 'project' && (tyeDebugConfiguration.services === undefined || tyeDebugConfiguration.services.includes(service.description.name)))) {
+            for (const replicaName of Object.keys(debuggedService.replicas)) {
+                await vscode.debug.startDebugging(
+                    folder,
+                    {
+                        name:`Tye Replica: ${replicaName}`,
+                        type:'coreclr',
+                        request:'attach',
+                        processId: `${debuggedService.replicas[replicaName].pid}`
+                    });
             }
         }
 
