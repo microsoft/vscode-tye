0ae55225a7d228145912cdba8f9353344f9ab98a
Phillip Hoff <phillip@orst.edu>
2020-11-13 16:04:32 -0800

    Cleanup strings and cancellation.

--- src/debug/tyeDebugConfigurationProvider.ts
+++ src/debug/tyeDebugConfigurationProvider.ts
@@ -1,5 +1,9 @@
 import * as vscode from 'vscode';
-import { TyeClient } from 'src/services/tyeClient';
+import * as nls from 'vscode-nls';
+import { TyeClient } from '../services/tyeClient';
+import { getLocalizationPathForFile } from '../util/localization';
+
+const localize = nls.loadMessageBundle(getLocalizationPathForFile(__filename));
 
 export interface TyeDebugConfiguration extends vscode.DebugConfiguration {
     services?: string[];
@@ -12,14 +16,14 @@ export class TyeDebugConfigurationProvider implements vscode.DebugConfigurationP
     async resolveDebugConfigurationWithSubstitutedVariables(folder: vscode.WorkspaceFolder | undefined, debugConfiguration: vscode.DebugConfiguration, token?: vscode.CancellationToken): Promise<vscode.DebugConfiguration | null | undefined> {
         const tyeDebugConfiguration = <TyeDebugConfiguration>debugConfiguration;
 
-        const services = await this.tyeClient.getServices();
+        const services = await this.tyeClient.getServices(token);
 
         for (const debuggedService of (services ?? []).filter(service => service.serviceType === 'project' && (tyeDebugConfiguration.services === undefined || tyeDebugConfiguration.services.includes(service.description.name)))) {
             for (const replicaName of Object.keys(debuggedService.replicas)) {
                 await vscode.debug.startDebugging(
                     folder,
                     {
-                        name:`Tye Replica: ${replicaName}`,
+                        name: localize('debug.tyeDebugConfigurationProvider.sessionName', 'Tye Replica: {0}', replicaName),
                         type:'coreclr',
                         request:'attach',
                         processId: `${debuggedService.replicas[replicaName].pid}`
