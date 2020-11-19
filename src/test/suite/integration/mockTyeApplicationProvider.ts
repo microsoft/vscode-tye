8230b70150c0261c80f056f4e35aac6fe98c3f9c
Phillip Hoff <phillip@orst.edu>
2020-11-18 16:20:30 -0800

    Pre-PR tweaks.

--- src/test/suite/integration/mockTyeApplicationProvider.ts
+++ src/test/suite/integration/mockTyeApplicationProvider.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { TyeApplication, TyeApplicationProvider } from "src/services/tyeApplicationProvider";
 import * as vscode from 'vscode';
 
--- src/test/suite/integration/mockTyeClient.ts
+++ src/test/suite/integration/mockTyeClient.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { TyeClient } from '../../../services/tyeClient';
 
 export class MockTyeClient implements TyeClient {
