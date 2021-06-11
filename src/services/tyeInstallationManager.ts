f9ce91d4775048f8debf2fb2353cb9eeb786296b
Phillip Hoff <phillip@orst.edu>
2021-06-11 14:23:38 -0700

    Add file header.

--- src/services/tyeInstallationManager.ts
+++ src/services/tyeInstallationManager.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import * as semver from 'semver';
 import { TyeCliClient } from './tyeCliClient';
 import * as nls from 'vscode-nls';
